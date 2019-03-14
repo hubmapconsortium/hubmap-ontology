999f153d190c0cee285df525c37a02359eae9161
SamuelFriedman <6232905+SamuelFriedman@users.noreply.github.com>
2019-03-14 00:04:06 -0700

    Read in UBERON ontology, select a term, make a branching, and write it out.

---
+++ owl_to_nx.py
@@ -0,0 +1,194 @@
+#!/usr/bin/python3
+#######################################################################
+#
+# This code takes an ontospy object and makes a Networkx graph
+#
+# Copyright, Samuel Friedman, Opto-Knowledge Systems, Inc. (OKSI), 2019
+#
+# Licensed under the MIT License.
+#
+# This code is for the NIH HuBMAP Consortium.
+#
+#######################################################################
+
+import networkx as nx
+import ontospy
+from urllib.parse import urldefrag
+import rdflib
+
+# Take ontology class and get its label
+def class_label(onto_class):
+    label_info = list(onto_class.rdflib_graph.subject_objects(predicate=rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#label')))
+    #print(onto_class.locale)
+    if len(label_info) != 0:
+        #print(label_info, )
+        return label_info[0][1].value
+    else:
+        return onto_class.locale
+
+# Take the ontology ID and get its label
+#def node_label(g,o, id):
+#    return class_label(o.get_class(list(g.nodes)
+def id_label(o, ont_id):
+    return class_label(o.get_class(ont_id)[0])
+
+def partof_subclassof_list(onto_class):
+    part_of = rdflib.term.URIRef("http://purl.obolibrary.org/obo/BFO_0000050")
+    some_values_from = rdflib.term.URIRef('http://www.w3.org/2002/07/owl#someValuesFrom')
+    subclassof_rdf = rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf')
+
+    subclassof_list = list(onto_class.rdflib_graph.objects(predicate=(subclassof_rdf)))
+    ps_list = [] #partof_subclassof_list
+    for subclassof in subclassof_list:
+        if type(subclassof) == rdflib.term.BNode:
+            bnode_objects = list(onto_class.rdflib_graph.objects(subclassof))
+            bnode_predicates = list(onto_class.rdflib_graph.predicates(subclassof))
+            #print(bnode_predicates)
+            if part_of in bnode_objects:
+                part_of_node = rdflib.term.URIRef(list(onto_class.rdflib_graph.objects(subclassof,some_values_from))[0])
+                
+                #g.add_edge(onto_class.locale, ontospy.core.utils.inferURILocalSymbol(part_of_node.toPython())[0], type='PartOf')
+                ps_list.append((ontospy.core.utils.inferURILocalSymbol(part_of_node.toPython())[0], onto_class.locale))
+    return ps_list
+
+
+# Create the graph
+g = nx.DiGraph(IRI="ext.owl")
+# Load the ontology
+o = ontospy.Ontospy()
+o.load_rdf("ext.owl")
+o.build_all()
+
+# Use a sample element
+rg_id = "UBERON_0000074"
+renal_glomerulus = o.get_class(rg_id)
+
+# Create graph from the ontology
+subclassof_string = 'SubClassOf'
+partof_string = 'PartOf'
+classassertion_string = 'ClassAssertion'
+for onto_class in o.all_classes:
+    g.add_node(onto_class.locale, type='Class')
+
+    for parent in onto_class.parents():
+        # This works for UBERON at least
+        g.add_edge(onto_class.locale, parent.locale, type=subclassof_string)
+
+        for instance in onto_class.instances:
+            start_url, fragment = urldefrag(instance)
+            g.add_edge(fragment, onto_class.locale, type=classassertion_string)
+
+# Check later if reversal is still necessary
+g = g.reverse(copy=False)
+g_orig = copy.copy(g)
+# Make a DAG only involving one node, but with all its ancestors and descendants
+#g_slim = g.subgraph(networkx.descendants(g, rg_id))
+#g_slim = networkx.compose(list(g.subgraph(networkx.descendants(g,rg_id)), g.subgraph(networkx.ancestors(g, rg_id))))                                           
+
+# Add partOf to the graph
+part_of = rdflib.term.URIRef("http://purl.obolibrary.org/obo/BFO_0000050")
+some_values_from = rdflib.term.URIRef('http://www.w3.org/2002/07/owl#someValuesFrom')
+subclassof_rdf = rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf')
+for onto_class in o.all_classes:
+    # Find more complex subClassOf objects that rdflib identifies through BNode elements
+    #onto_class.rdflib_graph.subect_objects(predicate=
+    subclassof_list = list(onto_class.rdflib_graph.objects(predicate=(subclassof_rdf)))
+    # Below needs to be replaced by function that is already defined
+    for subclassof in subclassof_list:
+        if type(subclassof) == rdflib.term.BNode:
+            bnode_objects = list(onto_class.rdflib_graph.objects(subclassof))
+            bnode_predicates = list(onto_class.rdflib_graph.predicates(subclassof))
+            #print(bnode_predicates)
+            if part_of in bnode_objects:
+                part_of_node = rdflib.term.URIRef(list(onto_class.rdflib_graph.objects(subclassof,some_values_from))[0])
+                
+                #g.add_edge(onto_class.locale, ontospy.core.utils.inferURILocalSymbol(part_of_node.toPython())[0], type='PartOf')
+                g.add_edge(ontospy.core.utils.inferURILocalSymbol(part_of_node.toPython())[0], onto_class.locale, type=partof_string)
+
+# Add weights to the graph
+for edge in g.edges:
+    edge_type = g.get_edge_data(*edge)['type']
+    edge_weight = 0
+    if edge_type == subclassof_string:
+        edge_weight = 1
+    elif edge_type == partof_string:
+        edge_weight = 2
+    g.get_edge_data(*edge)['weight'] = edge_weight
+
+# Make a slim version of the graph
+rg_tree_set = nx.descendants(g,rg_id) | nx.ancestors(g,rg_id)
+rg_tree_set.add(rg_id)
+g_slim = g.subgraph(rg_tree_set)
+
+# Make a maximum branching
+max_g_slim = nx.maximum_branching(g_slim)
+
+# create labels, nominally for plotting
+for node in max_g_slim:                  
+    g_slim_labels[node] = id_label(o,node)
+
+# Turn graph back into ontology
+o_slim = ontospy.Ontospy()
+o_slim_rdf_graph = rdflib.Graph()
+for node in max_g_slim:
+    # Find the appropriate class from the original ontology
+    new_o_class = o.get_class(node)[0]
+    # Substitue out the triple that was the partof that was problematic
+    # First, find pairs that should be added
+    ps_list = partof_subclassof_list(new_o_class)
+    # Then add in the new subclassof relationship
+    for ps_sub in ps_list:
+        superclass_rdf = rdflib.term.URIRef("http://purl.obolibrary.org/obo/"+ps_sub[0])
+        #new_o_class.rdflib_graph.add((ps_sub[1], subclassof_rdf, ps_sub[0]))
+        new_o_class.rdflib_graph.add((new_o_class.uri, subclassof_rdf, superclass_rdf))
+        #new_o_class.triples = o_slim.sparqlHelper.entityTriples(new_o_class.uri)
+    # Now add the class to the ontology
+    o_slim.all_classes += [new_o_class]
+o_slim.all_classes = sorted(o_slim.all_classes, key=lambda x: x.qname)
+
+
+# Now generate the string for serialization
+s_slim = ""
+for o_slim_class in o_slim.all_classes:
+    s_slim +=o_slim_class.rdf_source()
+for new_o_class in o_slim.all_classes:
+    o_slim_rdf_graph += new_o_class.rdflib_graph
+osrg = o_slim_rdf_graph.serialize(format="turtle")
+if isinstance(osrg, bytes):
+    osrg = osrg.decode('utf-8')
+s_slim = osrg
+# Write the string to a file
+slim_file = open("slim.ttl","w")
+slim_file.write(s_slim)
+slim_file.close()
+
+##############################
+# IGNORE BELOW
+##############################
+#
+## Add treeview edges
+#for onto_class in renal_glomerulus:
+#    # Find more complex subClassOf objects that rdflib identifies through BNode elements
+#    #onto_class.rdflib_graph.subect_objects(predicate=
+#    subclassof_list = list(onto_class.rdflib_graph.objects(predicate=(rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'))))
+#    for subclassof in subclassof_list:
+#        if type(subclassof) == rdflib.term.BNode:
+#            bnode_objects = list(onto_class.rdflib_graph.objects(subclassof))
+#            bnode_predicates = list(onto_class.rdflib_graph.predicates(subclassof))
+#            print(bnode_predicates)
+#            if part_of in bnode_objects:
+#                part_of_node = rdflib.term.URIRef(list(onto_class.rdflib_graph.objects(subclassof,some_values_from))[0])
+#                
+#                g.add_edge(onto_class.locale, ontospy.core.utils.inferURILocalSymbol(part.toPython())[0], type='PartOf')
+## Add treeview edges
+#for onto_class in renal_glomerulus:
+#    # Find more complex subClassOf objects that rdflib identifies through BNode elements
+#    #onto_class.rdflib_graph.subect_objects(predicate=
+#    subclassof_list = list(onto_class.rdflib_graph.objects(predicate=(rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'))))
+#    for subclassof in subclassof_list:
+#        if type(subclassof) == rdflib.term.BNode:
+#            bnode_objects = list(onto_class.rdflib_graph.objects(subclassof))
+#            bnode_predicates = list(onto_class.rdflib_graph.predicates(subclassof))
+#            #print(bnode_predicates)
+#            if part_of in bnode_objects:
+#                part_of_node = rdflib.term.URIRef(list(onto_class.rdflib_graph.objects(subclassof,some_values_from))[0])
