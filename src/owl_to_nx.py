4102081d034c1ca2562d7daab45ade986f42312a
Samuel Friedman <SamuelFriedman@users.noreply.github.com>
2019-04-23 13:58:33 -0700

    Eliminate unneeded terms and create a partonomy #7 #17

--- src/owl_to_nx.py
+++ src/owl_to_nx.py
@@ -261,9 +261,22 @@ if dot_files in owl_settings_dict:
 # Turn graph back into ontology
 #o_slim = ontospy.Ontospy()
 o_slim_rdf_graph = rdflib.Graph()
+o_partonomy_rdf_graph = rdflib.Graph()
 onClass = rdflib.term.URIRef('http://www.w3.org/2002/07/owl#onClass')
+obo_id_rdf = rdflib.term.URIRef('http://www.geneontology.org/formats/oboInOwl#id')
+label_rdf = rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#label')
+synonym_rdf = rdflib.term.URIRef('http://www.geneontology.org/formats/oboInOwl#hasExactSynonym')
+disjointwith_rdf = rdflib.term.URIRef('http://www.w3.org/2002/07/owl#disjointWith')
 partonomy = True
 for node in max_g_slim:
+    ################################
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Only user UBERON terms for now
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ################################
+    #if re.match(node,"UBERON") == None :
+    #    continue
+    
     # Find the appropriate class from the original ontology
     new_o_class_rdflib_graph = copy.deepcopy(o.get_class(node)[0].rdflib_graph)
     # Substitue out the triple that was the partof that was problematic
@@ -277,6 +290,11 @@ for node in max_g_slim:
             new_o_class_rdflib_graph.add((subclass_rdf, subclassof_rdf, superclass_rdf))
         else: # Partonomy is true
             new_o_class_rdflib_graph.add((subclass_rdf, part_of, superclass_rdf))
+    # Convert subclassof to partof for partonomy
+    if partonomy:
+        subclass_to_partof_list = list(new_o_class_rdflib_graph.triples((None,subclassof_rdf,None)))
+        for subclass_to_partof in subclass_to_partof_list:
+            new_o_class_rdflib_graph.add((subclass_to_partof[0],part_of,subclass_to_partof[2]))
     # remove those edges from max_g_slim
     # Needs to be in_edges because of how subClassOf works
     #for edge in removed_edges:
@@ -299,14 +317,74 @@ for node in max_g_slim:
     # not present in the slim ontology
     # Get the list of subject predicates for a wildcard match
     #new_o_class_rdflib_graph.subject_predicates(rdflib.term.URIRef("http://purl.obolibrary.org/obo/UBERON_"))
+    #for 
+    #check_term = list(new_o_class_rdflib_graph.subjects())[1]
+    #match_object = re.match("http://purl.obolibrary.org/obo/",str(check_term.encode())[2:-1])
+    #check_label = match_object.string[match_object.end():]
+    # part of. Skip the BNode elements
+    ps_list_2 = list(new_o_class_rdflib_graph.triples((None,part_of,None)))
+    ps_list_2.extend(list(new_o_class_rdflib_graph.triples((None,subclassof_rdf,None))))
+    non_bnode_list = [ triple for triple in ps_list_2 if type(triple[2]) != rdflib.term.BNode ]
+    removal_list = [ triple for triple in ps_list_2 if type(triple[2]) == rdflib.term.BNode ]
+    # Find only those elements that are NOT in the tree
+    for non_bnode in non_bnode_list:
+        match_object = re.match("http://purl.obolibrary.org/obo/",str(non_bnode[2].encode())[2:-1])
+        if match_object:
+            check_label = match_object.string[match_object.end():]
+            if check_label not in max_g_slim:
+                removal_list.extend([(non_bnode[0],part_of,non_bnode[2])])
+                removal_list.extend([(non_bnode[0],subclassof_rdf,non_bnode[2])])
+    # Eliminate the "disjoint with" terms
+    disjoint_list = list(new_o_class_rdflib_graph.triples((None,disjointwith_rdf,None)))
+    for disjoint_triple in disjoint_list:
+        match_object = re.match("http://purl.obolibrary.org/obo/",str(disjoint_triple[2].encode())[2:-1])
+        if match_object:
+            check_label = match_object.string[match_object.end():]
+            if check_label not in max_g_slim:
+                removal_list.extend([disjoint_triple])
+    for removal_triple in removal_list:
+        new_o_class_rdflib_graph.remove(removal_triple)
     # Now add the class to the ontology
     o_slim_rdf_graph += new_o_class_rdflib_graph
+    # For the partonomy, we only want a subset of the class.
+    # We want id, iri, parent, label, and altlabels (hasExactSynonym)
+    if partonomy:
+        triple_list = []
+        triple_types = [obo_id_rdf, label_rdf, synonym_rdf]
+        for triple_type in triple_types:
+            triple_list.extend(list(new_o_class_rdflib_graph.triples((None,triple_type,None))))
+        # part of. Skip the BNode elements
+        part_of_list = list(new_o_class_rdflib_graph.triples((None,part_of,None)))
+        non_bnode_list = [ triple for triple in part_of_list if type(triple[2]) != rdflib.term.BNode ]
+        # Find only those elements that are in the partonomy
+        for non_bnode in non_bnode_list:
+            match_object = re.match("http://purl.obolibrary.org/obo/",str(non_bnode[2].encode())[2:-1])
+            if match_object:
+                check_label = match_object.string[match_object.end():]
+                if check_label in max_g_slim:
+                    triple_list.extend([(non_bnode[0],rdflib.term.Literal("parent"),non_bnode[2])])
+        for triple in triple_list:
+            o_partonomy_rdf_graph.add(triple)
+        
 
 #o_slim.all_classes = sorted(o_slim.all_classes, key=lambda x: x.qname)
 
 # Add print out of the properties as well
 for o_property in o.all_properties:
-    o_slim_rdf_graph += o_property.rdflib_graph
+    o_property_graph = copy.deepcopy(o_property.rdflib_graph)
+    # Check to see if any UBERON terms have been added
+    removal_property_list = []
+    for o_property_triple in o_property_graph:
+        for triple_part in o_property_triple:
+            match_object = re.match("http://purl.obolibrary.org/obo/",str(triple_part.encode())[2:-1])
+            if match_object:
+                check_label = match_object.string[match_object.end():]
+                if re.match("UBERON",check_label):
+                    if check_label not in max_g_slim:
+                        removal_property_list.extend([o_property_triple])
+    for removal_property in removal_property_list:
+        o_property_graph.remove(removal_property)
+    o_slim_rdf_graph += o_property_graph
 
 # Now generate the string for serialization
 s_slim = ""
@@ -328,6 +406,20 @@ else:
 slim_file = open(slim_filename,"w")
 slim_file.write(s_slim)
 slim_file.close()
+
+if partonomy:
+    partonomy = o_partonomy_rdf_graph.serialize(format=serialization_format)
+    partonomy_json = o_partonomy_rdf_graph.serialize(format='json-ld')
+    if isinstance(partonomy_json, bytes):
+        partonomy_json = partonomy_json.decode('utf-8')
+    if isinstance(partonomy, bytes):
+        partonomy = partonomy.decode('utf-8')
+    p_file = open("ontologies/partonomy/partonomy.ttl","w")
+    p_file.write(partonomy)
+    p_file.close()
+    p_file = open("ontologies/partonomy/reifications/partonomy.jsonld.owl","w")
+    p_file.write(partonomy_json)
+    p_file.close()
 
 ##############################
 # IGNORE BELOW
