fe07e599e7b48aeb7825d00a1ac5a6f7c29d2e0d
Samuel Friedman <SamuelFriedman@users.noreply.github.com>
2019-05-28 09:16:14 -0700

    Functionalize the code to create the ontology. Needed for testing purposes. #10

--- src/owl_to_nx.py
+++ src/owl_to_nx.py
@@ -16,6 +16,7 @@ import yaml
 import ontotree
 
 def load_ontology(filename):
+    """Load the onology into memory"""
     # Load the ontology
     o = ontospy.Ontospy()
     o.load_rdf(filename)
@@ -24,18 +25,27 @@ def load_ontology(filename):
     o.build_properties() # We need this to export property names
     return o
 
-owl_settings = open("owl_settings.yml","r").read()
-owl_settings_dict = yaml.load(owl_settings)
-ontologies_string = "ontologies"
+def load_settings(owl_settings_filename):
+    """Read the settings file"""
+    owl_settings = open(owl_settings_filename,"r").read()
+    owl_settings_dict = yaml.load(owl_settings)
+    return owl_settings_dict
 
-o = load_ontology(owl_settings_dict["ontologies"]["UBERON_filename"])
-#o_cl = load_ontology(owl_settings_dict["ontologies"]["CL_filename"]) # Load CL
-o_graph = ontotree.ontotree()
-o_graph.load_terms(owl_settings_dict)
-o_graph.create_graph(o)
-o_graph.write_ont(o,owl_settings_dict, 0) # Write ontology
-o_graph.write_ont(o,owl_settings_dict, 1) # Write partonomy with XML
-o_graph.write_ont(o,owl_settings_dict, 2) # Write partonomy with JSON-LD
+def create_slim_ontology(owl_settings_filename):
+    """Create the ontology, compute its slim form, and write it"""
+    owl_settings_dict = load_settings(owl_settings_filename)
+    o = load_ontology(owl_settings_dict["ontologies"]["UBERON_filename"])
+    #o_cl = load_ontology(owl_settings_dict["ontologies"]["CL_filename"]) # Load CL
+    o_graph = ontotree.ontotree()
+    o_graph.load_terms(owl_settings_dict)
+    o_graph.create_graph(o)
+    o_graph.write_ont(o,owl_settings_dict, 0) # Write ontology
+    o_graph.write_ont(o,owl_settings_dict, 1) # Write partonomy with XML
+    o_graph.write_ont(o,owl_settings_dict, 2) # Write partonomy with JSON-LD
+    return o, o_graph
 
-
+if __name__ == "__main__":
+    owl_settings_filename = "owl_settings.yml"
+    create_slim_ontology(owl_settings_filename)
+    
 
