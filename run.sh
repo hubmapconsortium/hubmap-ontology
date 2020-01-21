6397c6e8f44ea278148194753d37d30b6eaaa19d
Bruce Herr II <bherr@indiana.edu>
2020-01-21 17:04:30 -0500

    Remove FMA from version control as it is too large.

--- run.sh
+++ run.sh
@@ -2,6 +2,13 @@
 
 mkdir -p docs
 
+if [ ! -e source_ontologies/fma.owl ]; then
+  echo "Please download the FMA Owl file from the URL below and save to source_ontologies/fma.owl"
+  echo
+  echo "https://bioportal.bioontology.org/ontologies/FMA"
+  exit -1
+fi
+
 python3 src/ccf_model.py
 python3 src/ccf_partonomy.py
 python3 src/ccf_full.py
