7ac45c5a3b4e679b7e9f9c9af4a88177c0a72e36
Bruce Herr II <bherr@indiana.edu>
2020-01-22 11:10:13 -0500

    Added a publishing script to push the latest ontology to github pages

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir -p docs
+mkdir -p dist
 
 python3 src/generate_ccf_model.py
-#python3 src/generate_ccf_partonomy.py
+python3 src/generate_ccf_partonomy.py
 python3 src/generate_ccf_reference_entities.py
 python3 src/generate_hubmap_spatial_entities.py
 python3 src/generate_ccf_full.py
