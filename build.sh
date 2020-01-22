d6581e6d0903986ad61b612d0f6380c4f3415af1
Bruce Herr II <bherr@indiana.edu>
2020-01-22 10:50:33 -0500

    Added code to generate a sample set of hubmap tissues registered with the RUI (WIP)

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 mkdir -p docs
 
 python3 src/generate_ccf_model.py
-python3 src/generate_ccf_partonomy.py
+#python3 src/generate_ccf_partonomy.py
 python3 src/generate_ccf_reference_entities.py
+python3 src/generate_hubmap_spatial_entities.py
 python3 src/generate_ccf_full.py
--- src/generate_ccf_full.py
+++ src/generate_ccf_full.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from rdflib import Graph
-from constants import CCF_FULL_ONTOLOGY, CCF_MODEL, CCF_PARTONOMY_RDF, CCF_REFERENCE_ENTITIES_RDF
+from constants import CCF_FULL_ONTOLOGY, CCF_MODEL, CCF_PARTONOMY_RDF, CCF_REFERENCE_ENTITIES_RDF, HUBMAP_ENTITIES_RDF
 
 
 g = Graph()
 g.parse(CCF_MODEL, format='xml')
 g.parse(CCF_PARTONOMY_RDF, format='xml')
 g.parse(CCF_REFERENCE_ENTITIES_RDF, format='xml')
+g.parse(HUBMAP_ENTITIES_RDF, format='xml')
 g.serialize(CCF_FULL_ONTOLOGY, format='xml')
