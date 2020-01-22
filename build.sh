e9805d5c9d8ad6b8362d0d789da235741729b3dc
Bruce Herr II <bherr@indiana.edu>
2020-01-22 12:17:56 -0500

    Added an example tissue section

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ python3 src/generate_ccf_partonomy.py
 python3 src/generate_ccf_reference_entities.py
 python3 src/generate_hubmap_spatial_entities.py
 python3 src/generate_ccf_full.py
+cp source_data/ccf-context.jsonld dist
