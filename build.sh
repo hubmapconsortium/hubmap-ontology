ce4a485be0ba831c73214e6af321206da14def0f
Bruce Herr II <bherr@indiana.edu>
2021-05-19 08:48:22 -0400

    Updated terms processing to add new ref organs / partonomy. Plus some cleanup.

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ cp -r source_objects dist/objects
 python3 src/generate_ccf_model.py
 python3 src/generate_ccf_partonomy.py
 python3 src/generate_ccf_reference_entities.py
-python3 src/generate_hubmap_spatial_entities.py
 python3 src/generate_ccf_full.py
 cp source_data/ccf-*context.jsonld dist
 cp src/ccf-partonomy.html src/ccf-partonomy.vg.json dist
--- src/generate_ccf_partonomy.py
+++ src/generate_ccf_partonomy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import bz2, glob, json
+import networkx as nx
 from csv import DictReader
 from operator import itemgetter
 from os import path
@@ -36,12 +37,6 @@ def get_term(term_str):
 
   return term[0] if len(term) > 0 else None
 
-def fix_term(term_str):
-  if term_str.startswith('FMAID'):
-    return term_str.replace(' ', '')
-  else:
-    return term_str
-
 def get_term_data(id, label, term, order, parent = None):
   return {
     '@id': term.iri,
@@ -49,7 +44,7 @@ def get_term_data(id, label, term, order, parent = None):
       { '@value': l } for l in term.hasExactSynonym
     ],
     'http://www.w3.org/2000/01/rdf-schema#label': [{
-      '@value': label
+      '@value': label.lower()
     }],
     'http://www.geneontology.org/formats/oboInOwl#id': [{
       '@value': id
@@ -58,6 +53,23 @@ def get_term_data(id, label, term, order, parent = None):
     'order': order
   }
 
+TERMS = [
+  'http://purl.obolibrary.org/obo/UBERON_0002097', # Skin
+  'http://purl.obolibrary.org/obo/UBERON_0000059', # Large Intestine
+  'http://purl.obolibrary.org/obo/UBERON_0000948', # Heart
+  'http://purl.obolibrary.org/obo/UBERON_0002113', # Kidney
+  # 'http://purl.obolibrary.org/obo/UBERON_0004538', # Kidney, L
+  # 'http://purl.obolibrary.org/obo/UBERON_0004539', # Kidney, R
+  'http://purl.obolibrary.org/obo/UBERON_0002106', # Spleen
+  'http://purl.obolibrary.org/obo/UBERON_0000955', # Allen Brain
+  'http://purl.obolibrary.org/obo/UBERON_0002048', # Lungs
+  'http://purl.obolibrary.org/obo/UBERON_0000029', # Lymph Node, L
+  # 'http://purl.obolibrary.org/obo/UBERON_0000029', # Lymph Node, R
+  'http://purl.obolibrary.org/obo/UBERON_0001270', # Pelvis
+  'http://purl.obolibrary.org/obo/UBERON_0002370', # Thymus
+  'http://purl.obolibrary.org/obo/UBERON_0002049' # Vasculature
+]
+
 with open(CCF_PARTONOMY_TERMS) as in_f:
   ccf_ns = Namespace(f'{CCF_NAMESPACE}#')
   ccf_part_of = ccf_ns.ccf_part_of
@@ -65,6 +77,7 @@ with open(CCF_PARTONOMY_TERMS) as in_f:
   g.namespace_manager.bind('ccf', ccf_ns, override=False)
   g.namespace_manager.bind('owl', OWL_NS, override=False)
   classes = {}
+  tree = nx.DiGraph()
 
   body = get_term('UBERON:0013702')
   terms = { body.iri: get_term_data('UBERON:0013702', 'body', body, 0, None) }
@@ -72,26 +85,17 @@ with open(CCF_PARTONOMY_TERMS) as in_f:
   for order, row in enumerate(DictReader(in_f)):
     if row['Cell-Type'] == 'Y':
       continue
-    parent = fix_term(row['Parent ID'])
-    child = fix_term(row['Ontology ID'])
+    parent = row['Parent ID']
+    child = row['Ontology ID']
 
     child_term = get_term(child)
     parent_term = get_term(parent)
 
     if child_term and parent_term:
       if child_term.iri == parent_term.iri:
-        print(f'Removed self-link: ${child}: ${child_term.iri}')
+        print(f'Removed self-link: {child}: {child_term.iri}')
       else:
-        g.add( (URIRef(child_term.iri), ccf_part_of, URIRef(parent_term.iri)) )
-
-        if parent_term.iri not in classes:
-          classes[parent_term.iri] = Class(URIRef(parent_term.iri), graph=g)
-        if child_term.iri not in classes:
-          classes[child_term.iri] = Class(URIRef(child_term.iri), graph=g)
-
-        parent_class = classes[parent_term.iri]
-        child_class = classes[child_term.iri]
-        child_class.subClassOf = [parent_class, Restriction(ccf_part_of, graph=g, someValuesFrom=parent_class)]
+        tree.add_edge(parent_term.iri, child_term.iri)
 
         child_label = row['HuBMAP Preferred Name'].strip()
         if not child_label:
@@ -102,8 +106,26 @@ with open(CCF_PARTONOMY_TERMS) as in_f:
           parent_label = parent_term.label[0]
           terms[parent_term.iri] = get_term_data(parent, parent_label, parent_term, order, body)
     elif len(parent+child.strip()) > 0:
-      print(f'Parent: {parent}, Child: {child}')
+      print(f'Parent: {parent} ({parent_term is None}), Child: {child} ({child_term is None})')
   
+  for term in TERMS:
+    tree.add_edge('root', term)
+
+  tree = nx.bfs_tree(tree, 'root')
+  for (parent_iri, child_iri) in tree.edges:
+    if parent_iri == 'root':
+      parent_iri = body.iri
+    g.add( (URIRef(child_iri), ccf_part_of, URIRef(parent_iri)) )
+
+    if parent_iri not in classes:
+      classes[parent_iri] = Class(URIRef(parent_iri), graph=g)
+    if child_iri not in classes:
+      classes[child_iri] = Class(URIRef(child_iri), graph=g)
+
+    parent_class = classes[parent_iri]
+    child_class = classes[child_iri]
+    child_class.subClassOf = [parent_class, Restriction(ccf_part_of, graph=g, someValuesFrom=parent_class)]
+
   for t in terms.values():
     term = URIRef(t['@id'])
     label = Literal(t['http://www.w3.org/2000/01/rdf-schema#label'][0]['@value'])
